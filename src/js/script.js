f97965de31bdc4e26a4fcff8bdeae341fbe7c4d6
Pawel Godula <bolomaster7@gmail.com>
2022-09-29 22:31:18 +0200

    starded function set value

--- src/js/script.js
+++ src/js/script.js
@@ -205,6 +205,7 @@
       console.log('constructor arguments:', element);
 
       thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
     }
     getElements(element) {
       const thisWidget = this;
@@ -213,6 +214,17 @@
       thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    setValue(value) {
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /*TODO Add validation */
+      if (thisWidget.value !== newValue && !isNaN(newValue)) {
+        thisWidget.value = newValue;
+      }
+      thisWidget.input.value = thisWidget.value;
     }
   }
 
